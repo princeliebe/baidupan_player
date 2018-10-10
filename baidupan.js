babd6040dde7b8b91d92b590a9615e1ac40f1c60
xihuanbanku <lianghaikun@qq.com>
2018-10-10 12:51:27 +0800

    支持video窗口提示

--- baidupan.js
+++ baidupan.js
@@ -11,44 +11,57 @@
 (function() {
     'use strict';
     $(".header-box").append("<div id='course1'>html1</div>");
+    $("#video-wrap-outer").append("<div id=\"html_player_enhance_tips\">tipps</div>");
+    $("#html_player_enhance_tips").attr("style", "position: absolute;z-index: 999999;padding: 10px;background: rgba(0,0,0,0.8);color:white;top: 50%;left: 50%;transform: translate(-50%,-50%);transition: all 500ms ease;display: none; -webkit-font-smoothing: subpixel-antialiased;font-family: 'microsoft yahei', Verdana, Geneva, sans-serif;-webkit-user-select: none;fontSize: 20px;");
+
     var tempRate = 0;
     document.onkeydown = function (event) {
             //按键X：减速播放 -0.1
             if (event.keyCode === 88) {
-                tempRate = videojs.getPlayers("video-player").html5player.tech_.playbackRate()
+                tempRate = player().playbackRate()
                 tempRate -= 0.1;
-                videojs.getPlayers("video-player").html5player.tech_.setPlaybackRate(tempRate)
+                player().setPlaybackRate(tempRate)
             }
             //按键C：加速播放 +0.1
             if (event.keyCode === 67) {
-                tempRate = videojs.getPlayers("video-player").html5player.tech_.playbackRate()
+                tempRate = player().playbackRate()
                 tempRate += 0.1;
-                videojs.getPlayers("video-player").html5player.tech_.setPlaybackRate(tempRate)
+                player().setPlaybackRate(tempRate)
             }
             //按键Z：正常速度播放
             if (event.keyCode === 90) {
                 tempRate = 1;
-                videojs.getPlayers("video-player").html5player.tech_.setPlaybackRate(tempRate)
+                player().setPlaybackRate(tempRate)
             }
             //按键1：播放*1
             if (event.keyCode === 49) {
-                tempRate = videojs.getPlayers("video-player").html5player.tech_.playbackRate()
+                tempRate = player().playbackRate()
                 tempRate = 1;
-                videojs.getPlayers("video-player").html5player.tech_.setPlaybackRate(tempRate)
+                player().setPlaybackRate(tempRate)
             }
             //按键2：播放*2
             if (event.keyCode === 50) {
-                tempRate = videojs.getPlayers("video-player").html5player.tech_.playbackRate()
+                tempRate = player().playbackRate()
                 tempRate = 2;
-                videojs.getPlayers("video-player").html5player.tech_.setPlaybackRate(tempRate)
+                player().setPlaybackRate(tempRate)
             }
             //按键3：播放*3
             if (event.keyCode === 51) {
-                tempRate = videojs.getPlayers("video-player").html5player.tech_.playbackRate()
+                tempRate = player().playbackRate()
                 tempRate = 3;
-                videojs.getPlayers("video-player").html5player.tech_.setPlaybackRate(tempRate)
+                player().setPlaybackRate(tempRate)
             }
+        settips(tempRate);
         $("#course1").html(tempRate);
+    }
+    function settips (tempRate) {
+        $('#html_player_enhance_tips').html(tempRate);
+        $('#html_player_enhance_tips').show();
+        $('#html_player_enhance_tips').fadeOut(1000);
+
+    }
+    function player() {
+        return videojs.getPlayers("video-player").html5player.tech_
     }
     // Your code here...
 })();
